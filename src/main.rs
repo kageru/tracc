78cb4bb1cbe341cf31f8f516bb84047376e38d4c
kageru <kageru@encode.moe>
2020-01-23 23:26:18 +0100

    Don’t use copied input handling from the examples

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::io;
 use termion::event::Key;
 use termion::raw::IntoRawMode;
+use termion::input::TermRead;
 use tui::backend::Backend;
 use tui::backend::TermionBackend;
 use tui::style::{Color, Style};
 use tui::widgets::*;
 use tui::Terminal;
-mod events;
-use events::{Event, Events};
 mod tracc;
 use tracc::Tracc;
 
@@ -18,45 +17,44 @@ pub enum Mode {
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
+    let mut inputs = io::stdin().keys();
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     let mut tracc = Tracc::new();
     terminal.hide_cursor()?;
     terminal.clear()?;
-    let events = Events::new();
     loop {
         refresh(&mut terminal, &tracc)?;
         // I need to find a better way to handle inputs. This is awful.
-        match events.next().expect("input ded?") {
-            Event::Input(input) => match tracc.mode {
-                Mode::Normal => match input {
-                    Key::Char('q') => break,
-                    Key::Char('j') => tracc.selection_down(),
-                    Key::Char('k') => tracc.selection_up(),
-                    Key::Char('o') => {
-                        tracc.insert();
-                        tracc.set_mode(Mode::Insert, &mut terminal)?;
+        let input = inputs.next().unwrap().expect("input ded?");
+        match tracc.mode {
+            Mode::Normal => match input {
+                Key::Char('q') => break,
+                Key::Char('j') => tracc.selection_down(),
+                Key::Char('k') => tracc.selection_up(),
+                Key::Char('o') => {
+                    tracc.insert();
+                    tracc.set_mode(Mode::Insert, &mut terminal)?;
+                }
+                Key::Char('a') => tracc.set_mode(Mode::Insert, &mut terminal)?,
+                Key::Char('A') => tracc.set_mode(Mode::Insert, &mut terminal)?,
+                Key::Char(' ') => tracc.toggle_current(),
+                // dd
+                Key::Char('d') => {
+                    if let Key::Char('d') = inputs.next().unwrap().unwrap() {
+                        tracc.remove_current()
                     }
-                    Key::Char('a') => tracc.set_mode(Mode::Insert, &mut terminal)?,
-                    Key::Char('A') => tracc.set_mode(Mode::Insert, &mut terminal)?,
-                    Key::Char(' ') => tracc.toggle_current(),
-                    // dd
-                    Key::Char('d') => {
-                        if let Event::Input(Key::Char('d')) = events.next().unwrap() {
-                            tracc.remove_current()
-                        }
-                    }
-                    _ => (),
-                },
-                Mode::Insert => match input {
-                    Key::Char('\n') => tracc.set_mode(Mode::Normal, &mut terminal)?,
-                    Key::Esc => tracc.set_mode(Mode::Normal, &mut terminal)?,
-                    Key::Backspace => tracc.current_pop(),
-                    Key::Char(x) => tracc.append_to_current(x),
-                    _ => (),
-                },
+                }
+                _ => (),
             },
-        }
+            Mode::Insert => match input {
+                Key::Char('\n') => tracc.set_mode(Mode::Normal, &mut terminal)?,
+                Key::Esc => tracc.set_mode(Mode::Normal, &mut terminal)?,
+                Key::Backspace => tracc.current_pop(),
+                Key::Char(x) => tracc.append_to_current(x),
+                _ => (),
+            },
+        };
     }
     Ok(())
 }
